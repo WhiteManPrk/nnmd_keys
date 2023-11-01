8e2e57ad1c2fd46b0ae26655322cc372c229b43f
Artem <39702941+WhiteManPrk@users.noreply.github.com>
2023-11-01 18:32:08 +0700

    Update hotkeys.js

--- hotkeys.js
+++ hotkeys.js
@@ -2,14 +2,14 @@ Lampa.Platform.tv();
 function log() {
         console.log.apply(console.log, arguments);
       }
-log('Hotkeys', 'Hotkeys 171 loaded');
+log('Hotkeys_2', 'Fucked Hotkeys loaded');
 
 function openPanel(element) {
 	if ('parseFloat(Lampa.Manifest.app_version)' >= '1.7') {
-        //log('Hotkeys', '1.7.0');
+        log('Hotkeys', '1.7.0');
 		Lampa.Utils.trigger(document.querySelector(element), 'click');
 	} else {
-        //log('Hotkeys', 'old version');
+        log('Hotkeys', 'old version');
 		document.querySelector(element).click();
 	}
 };
